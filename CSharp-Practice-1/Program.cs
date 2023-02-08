dcda079edf1df9175abd44b0f29a1a614ec922cb
Garrett-Shadow <vohmencevdanil@gmail.com>
2023-02-08 21:32:12 +0500

    Добавьте файлы проекта.

---
+++ CSharp-Practice-1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите строку");
+string st = Convert.ToString(Console.ReadLine());
+if (st.Length % 2 == 0)
+{
+    string sta = st.Substring(0, st.Length / 2);
+    string stb = st.Substring(st.Length / 2, st.Length - st.Length / 2);
+    string revsta = new string(sta.ToCharArray().Reverse().ToArray());
+    string revstb = new string(stb.ToCharArray().Reverse().ToArray());
+    string newst = revsta + revstb;
+    Console.WriteLine(newst);
+    Console.ReadKey();
+}
+else
+{
+    string stc = st;
+    string revst = new string(st.ToCharArray().Reverse().ToArray());
+    string newst = revst + stc;
+    Console.WriteLine(newst);
+    Console.ReadKey();
+}
